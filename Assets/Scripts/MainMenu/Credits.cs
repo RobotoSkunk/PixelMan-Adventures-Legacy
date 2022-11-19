bfbec592c939c21a456dd887775ea028475f4790
RobotoSkunk <contact@robotoskunk.com>
2022-11-18 21:57:56 -0600

    Added missing translations and improved Credits code

--- Assets/Scripts/MainMenu/Credits.cs
+++ Assets/Scripts/MainMenu/Credits.cs
@@ -2,40 +2,25 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
-using TMPro;
-
 
 namespace RobotoSkunk.PixelMan.UI.MainMenu {
 	public class Credits : MonoBehaviour, IBeginDragHandler, IEndDragHandler {
 		public ScrollRect scrollRect;
-		public TextMeshProUGUI credits;
-		public float speed, padding;
+		public float speed;
+
 		bool dragging = false;
+		RectTransform content { get => scrollRect.content; }
+		RectTransform viewport { get => scrollRect.viewport; }
 
 
-		float contentHeight { get => scrollRect.content.rect.height + padding; }
-		float delta { get => scrollRect.viewport.rect.height / contentHeight; }
-
-		float bottom { get => -contentHeight * delta; }
-		float top { get => contentHeight; }
-
-
-		private void Start() => SetContentPos(bottom);
+		private void Start() => content.anchoredPosition = new Vector2(0f, -viewport.rect.height);
 
 		private void Update() {
-			if (delta >= 1f) {
-				scrollRect.verticalNormalizedPosition = 0.5f;
-				return;
-			}
-			if (!dragging) scrollRect.verticalNormalizedPosition += Time.deltaTime * speed * delta;
+			if (!dragging) content.anchoredPosition += new Vector2(0f, speed);
+			float pos = content.anchoredPosition.y;
 
-			if (scrollRect.content.anchoredPosition.y > top) SetContentPos(bottom);
-			else if (scrollRect.content.anchoredPosition.y < bottom) SetContentPos(top);
-		}
-
-		void SetContentPos(float y) {
-			Vector2 pos = scrollRect.content.anchoredPosition; pos.y = y;
-			scrollRect.content.anchoredPosition = pos;
+			if (pos > content.rect.height) content.anchoredPosition = new(0f, -viewport.rect.height);
+			else if (pos < -viewport.rect.height) content.anchoredPosition = new Vector2(0f, content.rect.height);
 		}
 
 
