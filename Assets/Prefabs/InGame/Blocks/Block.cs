58f8655b804106e97332feb3a1712c0c26f141b1
RobotoSkunk <contact@robotoskunk.com>
2023-01-24 15:02:53 -0600

    Preparing everything for Blocks tiles

--- Assets/Prefabs/InGame/Blocks/Block.cs
+++ Assets/Prefabs/InGame/Blocks/Block.cs
@@ -3,16 +3,14 @@ using UnityEngine;
 
 namespace RobotoSkunk.PixelMan.Gameplay {
 	public class Block : MonoBehaviour {
+		public struct BlockData {
+			public Sprite sprite;
+			public float angle;
+		}
+
 		public InGameObjectBehaviour behaviour;
 		public SpriteRenderer spriteRenderer;
 		public Sprite defSpr;
-
-
-		public void SetSkin(int index) {
-			if (index >= 0 && index < behaviour.options.skins.Length)
-				spriteRenderer.sprite = behaviour.options.skins[index];
-			else
-				spriteRenderer.sprite = defSpr;
-		}
+		public BlockData[] data = new BlockData[47];
 	}
 }
