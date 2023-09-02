61fb491d0c6b9bf41d25f22c57ccc93f35338aa9
RobotoSkunk <contact@robotoskunk.com>
2023-09-02 12:15:15 -0600

    Improved Coin code

--- Assets/Prefabs/InGame/Coin/Coin.cs
+++ Assets/Prefabs/InGame/Coin/Coin.cs
@@ -20,8 +20,10 @@ using UnityEngine;
 
 
 
-namespace RobotoSkunk.PixelMan.Gameplay {
-	public class Coin : GameObjectBehaviour {
+namespace RobotoSkunk.PixelMan.Gameplay
+{
+	public class Coin : GameObjectBehaviour
+	{
 		public Animator animator;
 		public SpriteRenderer spriteRenderer;
 		public ParticleSystem particles;
@@ -30,9 +32,13 @@ namespace RobotoSkunk.PixelMan.Gameplay {
 		float delta = 0f;
 
 
-		public void OnGameState(bool isEditor) {
-			if (isEditor) particles.Stop();
-			else particles.Play();
+		public void OnGameState(bool isEditor)
+		{
+			if (isEditor) {
+				particles.Stop();
+			} else {
+				particles.Play();
+			}
 
 			if (isEditor) {
 				animator.speed = 0f;
@@ -40,38 +46,48 @@ namespace RobotoSkunk.PixelMan.Gameplay {
 			}
 		}
 
-		protected override void OnGameResetObject() {
+		protected override void OnGameResetObject()
+		{
 			collected = false;
 			Globals.gotCoin = false;
 
 			UpdateCoin(true);
 		}
 
-		private void Update() {
+
+		private void Update()
+		{
 			animator.speed = (!Globals.onPause).ToInt();
 
 			UpdateCoin();
 		}
 
-		private void OnTriggerEnter2D(Collider2D other) {
+		private void OnTriggerEnter2D(Collider2D other)
+		{
 			if (other.gameObject.CompareTag("Player")) {
 				collected = true;
 				Globals.gotCoin = true;
 			}
 		}
 
-		void UpdateCoin(bool force = false) {
+
+		void UpdateCoin(bool force = false)
+		{
 			if (collected) {
 				delta += Time.deltaTime;
 				delta = Mathf.Clamp01(delta);
 
 				animator.SetBool("Collected", true);
 
-				if (delta >= 0.5f && !particles.isStopped) particles.Stop();
+				if (delta >= 0.5f && !particles.isStopped) {
+					particles.Stop();
+				}
 				return;
 			}
 
-			if (!force) return;
+			if (!force) {
+				return;
+			}
 
 			spriteRenderer.color = Color.white;
 			animator.speed = 1f;
